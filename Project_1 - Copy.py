7d821d26a87f012bd847ecd7c4dc6f0ebeb8933f
Nosrat-Jahan <nishubd211@gmail.com>
2026-01-09 23:46:32 +0600

    Add files via upload

---
+++ Project_1 - Copy.py
@@ -0,0 +1,37 @@
+from PyPDF2 import PdfMerger
+
+AllPDF = ["1.pdf", "2.pdf"]
+
+OurMerger = PdfMerger()
+
+for NewPDF in AllPDF:
+    OurMerger.append(NewPDF)
+
+OurMerger.write("Nishu.pdf")
+OurMerger.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
